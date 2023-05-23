8f8a978476e330e5af77d191cdc55a238550fb6a
Cipher Phantom <oyepitan10@gmail.com>
2023-05-24 00:25:12 +0100

    Added the header, main and Authors files

---
+++ main.c
@@ -0,0 +1,157 @@
+#include "main.h"
+
+/**
+  * tokens_to_arr - This function creates an array of strings from tokens
+  * @lineptr_copy: This holds a copy of string to be tokenized
+  * @num_tokens: The number of tokens present in the string.
+  * @delim: The delimiter to be used.
+  * Return: Pointer to Pointer of strings or NULL.
+  */
+char **tokens_to_arr(char *lineptr_copy, int num_tokens, const char *delim)
+{
+	char **args = NULL, *token = NULL;
+	int i = 0;
+
+	args = malloc(sizeof(char *) * (num_tokens + 1));
+	if (!args)
+	{
+		free_func(&lineptr_copy, 0);
+		perror("line_to_args");
+		return (NULL);
+	}
+
+	token = _strtok(lineptr_copy, delim);
+	for (i = 0; token != NULL; i++)
+	{
+		args[i] = malloc(sizeof(char) * (_strlen(token) + 1));
+		if (!args[i])
+		{
+			free_func(&lineptr_copy, 0);
+			free_func(args, i);
+			perror("line_to_args");
+			return (NULL);
+		}
+
+		_strcpy(args[i], token);
+		token = _strtok(NULL, delim);
+	}
+	args[num_tokens] = NULL;
+	return (args);
+}
+
+/**
+  * line_to_args - This function returns the args in the string passed.
+  * @lineptr: This holds the string(line) to be split to args
+  * @delim: The delimiter to be used.
+  * Return: Pointer to Pointer of strings or NULL.
+  */
+char **line_to_args(char *lineptr, const char *delim)
+{
+	char *lineptr_copy = NULL, *token = NULL, **args = NULL;
+	int num_tokens = 0;
+
+	lineptr_copy = _strdup(lineptr);
+	if (!lineptr_copy)
+	{
+		perror("line_to_args");
+		return (NULL);
+	}
+	token = _strtok(lineptr, delim);
+	if (!token)
+	{
+		free_func(&lineptr_copy, 0);
+		return (NULL);
+	}
+
+	while (token)
+	{
+		num_tokens++;
+		token = _strtok(NULL, delim);
+	}
+
+	args = tokens_to_arr(lineptr_copy, num_tokens, delim);
+	free_func(&lineptr_copy, 0);
+	return (args);
+}
+
+
+/**
+  * emul_tty - This function loopss for the shell
+  * @lineptr: This holds the string(line) to be split to args
+  * @argv: Pointer to pointer of strings
+  * @envs: A NULL-terminated arr of str containing the current environment
+  */
+
+void emul_tty(char **lineptr, char **argv, char **envs)
+{
+	ssize_t nchars = 0;
+	char **args = NULL, *prompt = "($) ", delim[] = " \n\t";
+	size_t n = 0;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(1, prompt, _strlen(prompt));
+		errno = 0;
+		nchars = _getline(lineptr, &n, STDIN_FILENO);
+
+		if (nchars == -1)
+		{
+			if (errno)
+				perror("main");
+			if (isatty(STDIN_FILENO))
+				write(1, "\n", 1);
+			break;
+		}
+		cmd_counter(1);
+		args = line_to_args(*lineptr, delim);
+		if (!args)
+			continue;
+		if (check_builtin(args, envs, argv, lineptr))
+			free_func(args, -1);
+		else
+		{
+			execmd(argv, args);
+			free_func(args, -1);
+		}
+	}
+}
+
+/**
+  *stor_pid - This stores the initial pid of the program
+  *Return: The PID
+  *
+  */
+
+pid_t stor_pid(void)
+{
+	static pid_t the_pid;
+
+	if (!the_pid)
+		the_pid = getpid();
+	return (the_pid);
+}
+
+
+/**
+  * main - This program mimics the UNIX shell.
+  * @argc: Number of arguments passed in
+  * @argv: A NULL-teminated array of strings passed to CLI.
+  * @envs: A NULL-terminated arr of str containint the current environment
+  * Return: Exit status code.
+  */
+int main(int __attribute__((unused))argc, char *argv[], char **envs)
+{
+	char *lineptr = NULL;
+	Node *head = getenv_list(envs);
+
+	stor_pid();
+	while (1)
+	{
+		emul_tty(&lineptr, argv, envs);
+		break;
+	}
+	free_func(&lineptr, 0);
+	free_linked_list(head);
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,87 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <limits.h>
+
+extern int errno;
+extern char **environ;
+
+/**
+  * struct built_in - A structure keeping track of built-ins
+  * @exe: Name of built_in
+  * @func: Function to handle built-in
+  */
+typedef struct built_in
+{
+	char *exe;
+	int (*func)(char **args, char **env);
+} built_in;
+
+/**
+  * struct Node - A structure keeping track of env nodes
+  * @name: Name of Node
+  * @value: Value of Node
+  * @next: Pointer to next node
+  */
+typedef struct Node
+{
+	char *name;
+	char *value;
+	struct Node *next;
+} Node;
+
+void free_func(char **ptr, int length);
+extern int cmd_count;
+
+void execmd(char **argv, char **args);
+char **split_string(char *str, char *delim);
+char **get_PATH(void);
+char *digTostr(long num);
+
+
+char *_strchr(char *s, char c);
+int _strlen(const char *s);
+int _strcmp(char *s1, char *s2);
+int _strncmp(char *s1, char *s2, size_t n);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strdup(const char *s);
+
+
+
+int exit_shell(char **args, char **env, char **, char **);
+int print_env(char **args, char **env);
+int check_builtin(char **args, char **envs, char **, char **);
+int set_env(char **args, char **env);
+int unset_env(char **args, char **env);
+char *_getenv(char *env_name);
+void print_node(Node *ptr);
+Node *getenv_list(char **environ);
+void free_node(Node *ptr);
+char *_strtok(char *str, const char *delim);
+ssize_t _getline(char **lineptr, size_t *n, int fd);
+void free_linked_list(Node *ptr);
+int cmd_counter(int incr);
+void custom_err(char *command, char **argv, char **, int);
+char *_which(char **args, char **path_arr);
+char *concat_str(const unsigned int n, ...);
+char *_strncpy(char *dest, char *src, int n);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_memcpy(void *dest, void *src, unsigned int n);
+int _atoi_cust(char *s);
+pid_t stor_pid(void);
+char *_gEnv(char *name, char **envs);
+int _cd(char **args, char **envs, char **argv);
+
+#endif
